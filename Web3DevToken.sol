ff1f2d65dcb350f84256366005f0c2fc14a4738d
Ronie Steiner <ronie.steiner.moreira@gmail.com>
2024-10-02 14:25:48 -0300

    Update Web3DevToken.sol

--- Web3DevToken.sol
+++ Web3DevToken.sol
@@ -1,65 +1,60 @@
-// SPDX-License-Identifier: MIT 
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Web3DevToken2 is ERC20 {
+contract Web3DevToken2 is ERC20, Ownable {
     uint256 inviteAmount = 1 * 10 ** decimals();
     uint256 constant invitesPerAccount = 4;
-    mapping(address => bool) members;
-    address[] public memberAddresses;
+    
+    struct Member {
+        address whoInvited;
+        bool isMember;
+        uint invites;
+        bool isBanned;
+        uint lastInvite;
+        uint memberSince;
+    }
 
+    mapping(address => Member) public members;
 
     constructor() ERC20("Web3DevToken2", "W3DT") {
         _mint(msg.sender, invitesPerAccount);
-        members[msg.sender] = true;
-        memberAddresses.push(msg.sender);
+        members[msg.sender] = Member(msg.sender, true, invitesPerAccount, false,0,block.timestamp);
     }
 
     function invite(address account) public {
-        require(balanceOf(msg.sender) > 0, "Only members can invite!");
+        require(members[msg.sender].isMember, "Only members can invite!");
+        require(members[msg.sender].isBanned == false, "Banned members cannot invite!");
         require(balanceOf(msg.sender) > 1, "You run out of invites!");
-        require(account != msg.sender, "You can't invite yourself!" );
-        require(balanceOf(account) == 0, "The account you are trying to invite is already a member.");
+        require(account != msg.sender, "You can't invite yourself!");
+        require(members[account].isMember == false , "The account that you are trying to invite is already a member.");
+        require(block.timestamp >= members[msg.sender].lastInvite + 1 days);
 
         _burn(msg.sender, 1);
+        members[msg.sender].invites - 1;
         _mint(account, invitesPerAccount);
-        members[account] = true;
-        memberAddresses.push(account);
+        members[account] = Member(msg.sender, true, invitesPerAccount, false,0,block.timestamp);
+        members[msg.sender].lastInvite = block.timestamp
     }
 
-    function checkMember(address account) view public returns(bool) {
-        return members[account];
+    function checkMember(address account) public view returns (bool) {
+        require(members[account].isActive, "This address is not a member");
+        require(members[account].isBanned == false, "This address is banned from the community");
+        return true
     }
 
-    function transfer(address, uint256) public pure override returns(bool) {
+    function transfer(address, uint256) public pure override returns (bool) {
         revert("Transfers are disable!");
     }
 
-    function membersQuantity() public view returns (uint256) {
-        return memberAddresses.length;
+    function banAccount(address account) public onlyOwner {
+        require(members[account].isActive, "This address is not an member");
+        members[account].isBanned = true; 
     }
 
-    function membersList(uint256 startIndex, uint256 endIndex) public view returns (address[] memory) {
-        require(
-            endIndex > startIndex,
-            "End index must be greater than start index."
-        );
-        require(
-            endIndex <= memberAddresses.length,
-            "End index out of bounds. To know the number of indices, use the function membersQuantity."
-        );
-        require(
-            (endIndex - startIndex) < 100,
-            "The maximum number of indices to show is 100"
-        );
-
-        uint256 length = endIndex - startIndex;
-        address[] memory partialList = new address;
-
-        for (uint256 i = 0; i < length; i++) {
-            partialList[i] = memberAddresses[startIndex + i];
-        }
-
-        return partialList;
+    function unBanAccount(address account) public onlyOwner {
+        require(members[account].isActive, "This address is not an member");
+        members[account].isBanned = false; 
     }
 }
